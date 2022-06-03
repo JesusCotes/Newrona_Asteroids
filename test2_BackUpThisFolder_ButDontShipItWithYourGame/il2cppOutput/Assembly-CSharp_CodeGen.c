26a02349197539d78c6a433d8b5b9f30f2abd2a4
JesusCotes <jmcgarcia93@gmail.com>
2022-06-03 02:48:20 -0500

    Add APK to project

---
+++ test2_BackUpThisFolder_ButDontShipItWithYourGame/il2cppOutput/Assembly-CSharp_CodeGen.c
@@ -0,0 +1,257 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+// 0x00000001 System.Void Asteroids::Start()
+extern void Asteroids_Start_mF50AE073495D448543D63DC934F15FA650913343 (void);
+// 0x00000002 System.Void Asteroids::Update()
+extern void Asteroids_Update_m2BBDCF1CA253802F57EEAC318E37621E0DBEF086 (void);
+// 0x00000003 System.Void Asteroids::OnCollisionEnter(UnityEngine.Collision)
+extern void Asteroids_OnCollisionEnter_mE3C57C903E08BFCF8B60F4ECE77336E3B01839DD (void);
+// 0x00000004 System.Void Asteroids::.ctor()
+extern void Asteroids__ctor_m84B0074109382C5759B641EA1109FC8836F86968 (void);
+// 0x00000005 System.Void miniAsteroids::Start()
+extern void miniAsteroids_Start_mFFAE6FBD20FD959D5F32561EB2BAE364CDD92362 (void);
+// 0x00000006 System.Void miniAsteroids::Update()
+extern void miniAsteroids_Update_mFE654B7B615782E291B6B0E4B59921884BBE994D (void);
+// 0x00000007 System.Void miniAsteroids::OnCollisionEnter(UnityEngine.Collision)
+extern void miniAsteroids_OnCollisionEnter_m6A46746D06ABAF070D848738D583D84DEF72B645 (void);
+// 0x00000008 System.Void miniAsteroids::.ctor()
+extern void miniAsteroids__ctor_mA7432E07B81A3BC116C369E63B80603B73613687 (void);
+// 0x00000009 System.Void Explosion::Start()
+extern void Explosion_Start_m938A49A4FF38A4F64F2D3B4420B2FADC03CB14D3 (void);
+// 0x0000000A System.Void Explosion::Update()
+extern void Explosion_Update_mAB50C3AC1611DB0676162E0D77F65DFDC08AD249 (void);
+// 0x0000000B System.Void Explosion::OnCollisionEnter(UnityEngine.Collision)
+extern void Explosion_OnCollisionEnter_m0E121DA86CFB0C54475C615D41A0F33040E4FCCD (void);
+// 0x0000000C System.Void Explosion::.ctor()
+extern void Explosion__ctor_mCE002252DB377B0FC990478B1F6EE4414E832403 (void);
+// 0x0000000D System.Void GameManager::Start()
+extern void GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2 (void);
+// 0x0000000E System.Void GameManager::Update()
+extern void GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41 (void);
+// 0x0000000F System.Void GameManager::nextLevel()
+extern void GameManager_nextLevel_m94AE3B92663EEB2FD72056E0B4964D8F020737F8 (void);
+// 0x00000010 System.Void GameManager::.ctor()
+extern void GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368 (void);
+// 0x00000011 System.Void GameManager::.cctor()
+extern void GameManager__cctor_m17C19843AAD22B2A037A6F670EEE26E7C2C35D0B (void);
+// 0x00000012 System.Void LifePlayer::Start()
+extern void LifePlayer_Start_m25392670802B17F57090D0B05AE1C912AADAC7E1 (void);
+// 0x00000013 System.Void LifePlayer::Update()
+extern void LifePlayer_Update_m40EFCCAB30CFFB98C3AA7397011F1851E309497B (void);
+// 0x00000014 System.Void LifePlayer::OnCollisionEnter(UnityEngine.Collision)
+extern void LifePlayer_OnCollisionEnter_m6B431ED033487AC102244D8D322F32EB9F96BACC (void);
+// 0x00000015 System.Void LifePlayer::.ctor()
+extern void LifePlayer__ctor_mDD2D2940B64B2ABB14CB81913D33F9B7BFBA81C7 (void);
+// 0x00000016 System.Void CameraPointer::Update()
+extern void CameraPointer_Update_mF19D284A593E4627CD4F22A58C11FB87B6627FF4 (void);
+// 0x00000017 System.Void CameraPointer::.ctor()
+extern void CameraPointer__ctor_m5C4B443EB7C8A518306BF27BD947D938FDF5162D (void);
+// 0x00000018 System.Void CardboardStartup::Start()
+extern void CardboardStartup_Start_m35E3138C09D62411C4C8503DD4D47DA2AF9BF6E2 (void);
+// 0x00000019 System.Void CardboardStartup::Update()
+extern void CardboardStartup_Update_m4D4D770B4F4BA807AB64DB40B6730226BB821883 (void);
+// 0x0000001A System.Void CardboardStartup::.ctor()
+extern void CardboardStartup__ctor_m4C459294D28BDCFC64FECC657FB860844F52D9F7 (void);
+// 0x0000001B System.Void ObjectController::Start()
+extern void ObjectController_Start_m50CBBCE72AB6C2A5C8BC1977D520CF3856A91E89 (void);
+// 0x0000001C System.Void ObjectController::TeleportRandomly()
+extern void ObjectController_TeleportRandomly_m45F23374832369044CBD4F60F4F976D33C97C181 (void);
+// 0x0000001D System.Void ObjectController::OnPointerEnter()
+extern void ObjectController_OnPointerEnter_m5D028066A6B28C2770924AE0C5A2A926E807FC42 (void);
+// 0x0000001E System.Void ObjectController::OnPointerExit()
+extern void ObjectController_OnPointerExit_mB2F968E3432A4B5E90ABFCE7E6B025A39041809F (void);
+// 0x0000001F System.Void ObjectController::OnPointerClick()
+extern void ObjectController_OnPointerClick_m42836A892DD52FE0C121F5E37231BF198052017E (void);
+// 0x00000020 System.Void ObjectController::SetMaterial(System.Boolean)
+extern void ObjectController_SetMaterial_mF8B3638C2382300F585FCFAE6EC5626AAE5EC772 (void);
+// 0x00000021 System.Void ObjectController::.ctor()
+extern void ObjectController__ctor_mA008C8DFDEF1B6A05926338921FE1FD30BCEA9BC (void);
+// 0x00000022 System.Boolean VrModeController::get__isScreenTouched()
+extern void VrModeController_get__isScreenTouched_mD72F14326F1D19F3AC970FD0ED9D0C5CD5C6CFB5 (void);
+// 0x00000023 System.Boolean VrModeController::get__isVrModeEnabled()
+extern void VrModeController_get__isVrModeEnabled_m8D8D68536E09FF2DEAD3795AF30A57BA6DF7C95F (void);
+// 0x00000024 System.Void VrModeController::Start()
+extern void VrModeController_Start_m10BF77FE8B825D9640800D5DF7EF791A391FA49B (void);
+// 0x00000025 System.Void VrModeController::Update()
+extern void VrModeController_Update_m43F984D5BF13F2C545B508D93645C1C3AC9262D5 (void);
+// 0x00000026 System.Void VrModeController::EnterVR()
+extern void VrModeController_EnterVR_m5FD79BF38C6AF621AF9992378BC5C3618FDE5EFB (void);
+// 0x00000027 System.Void VrModeController::ExitVR()
+extern void VrModeController_ExitVR_m1FF25DFE5B3BC15CEDE5C42F56CC399693867397 (void);
+// 0x00000028 System.Collections.IEnumerator VrModeController::StartXR()
+extern void VrModeController_StartXR_m57D80B4AD6B28A7C4E36052563B6227E824CDDB4 (void);
+// 0x00000029 System.Void VrModeController::StopXR()
+extern void VrModeController_StopXR_m0338E8D8035C06E1A46F17C16BBC7933EE9CAA3A (void);
+// 0x0000002A System.Void VrModeController::.ctor()
+extern void VrModeController__ctor_m94505259847FB7C54346D7CF02AD85793393D141 (void);
+// 0x0000002B System.Void VrModeController/<StartXR>d__10::.ctor(System.Int32)
+extern void U3CStartXRU3Ed__10__ctor_mDA431E1365B30DA2F64F1E7875CD387F2231E138 (void);
+// 0x0000002C System.Void VrModeController/<StartXR>d__10::System.IDisposable.Dispose()
+extern void U3CStartXRU3Ed__10_System_IDisposable_Dispose_m3DC1735AA5EC0D61A72803BB3B33B9499A373336 (void);
+// 0x0000002D System.Boolean VrModeController/<StartXR>d__10::MoveNext()
+extern void U3CStartXRU3Ed__10_MoveNext_m9A6340456306ACFADD0D458946C82448A862E68B (void);
+// 0x0000002E System.Object VrModeController/<StartXR>d__10::System.Collections.Generic.IEnumerator<System.Object>.get_Current()
+extern void U3CStartXRU3Ed__10_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mD0B4CE8CBE980FD132B19F40FF2172502B1D0566 (void);
+// 0x0000002F System.Void VrModeController/<StartXR>d__10::System.Collections.IEnumerator.Reset()
+extern void U3CStartXRU3Ed__10_System_Collections_IEnumerator_Reset_mD99AEFDB38CFAC11731CDB371647891816288600 (void);
+// 0x00000030 System.Object VrModeController/<StartXR>d__10::System.Collections.IEnumerator.get_Current()
+extern void U3CStartXRU3Ed__10_System_Collections_IEnumerator_get_Current_mD84D9ACDB0A04C619509BA51590975DB84C30B0F (void);
+// 0x00000031 System.Void MouseLook::Start()
+extern void MouseLook_Start_m08FD16A47A96624540CD1B8EBA5FFAA1FD75E686 (void);
+// 0x00000032 System.Void MouseLook::Update()
+extern void MouseLook_Update_m18C0A114A78C19B888A9F74CA1FE5A837DF9E42A (void);
+// 0x00000033 System.Void MouseLook::.ctor()
+extern void MouseLook__ctor_m4C8A79A3CB8D46D3E22E3AB1F645D41ED589312B (void);
+// 0x00000034 System.Void ShootingProjectile::start()
+extern void ShootingProjectile_start_m8D5C605DDEC776B806C1D889CC103091E8B5E858 (void);
+// 0x00000035 System.Void ShootingProjectile::Update()
+extern void ShootingProjectile_Update_mB492E4C25D212E97A17A8FE715E25501B5BF8ED6 (void);
+// 0x00000036 System.Void ShootingProjectile::.ctor()
+extern void ShootingProjectile__ctor_mB3B10D0E79EA4841F424C1CFA2563C8D9F67C861 (void);
+static Il2CppMethodPointer s_methodPointers[54] = 
+{
+	Asteroids_Start_mF50AE073495D448543D63DC934F15FA650913343,
+	Asteroids_Update_m2BBDCF1CA253802F57EEAC318E37621E0DBEF086,
+	Asteroids_OnCollisionEnter_mE3C57C903E08BFCF8B60F4ECE77336E3B01839DD,
+	Asteroids__ctor_m84B0074109382C5759B641EA1109FC8836F86968,
+	miniAsteroids_Start_mFFAE6FBD20FD959D5F32561EB2BAE364CDD92362,
+	miniAsteroids_Update_mFE654B7B615782E291B6B0E4B59921884BBE994D,
+	miniAsteroids_OnCollisionEnter_m6A46746D06ABAF070D848738D583D84DEF72B645,
+	miniAsteroids__ctor_mA7432E07B81A3BC116C369E63B80603B73613687,
+	Explosion_Start_m938A49A4FF38A4F64F2D3B4420B2FADC03CB14D3,
+	Explosion_Update_mAB50C3AC1611DB0676162E0D77F65DFDC08AD249,
+	Explosion_OnCollisionEnter_m0E121DA86CFB0C54475C615D41A0F33040E4FCCD,
+	Explosion__ctor_mCE002252DB377B0FC990478B1F6EE4414E832403,
+	GameManager_Start_m87A71D65F3171A58DBDDBFB03832ADA65643D0E2,
+	GameManager_Update_m7F29D8E933B8D21D2E67507979C0F12ACF87BB41,
+	GameManager_nextLevel_m94AE3B92663EEB2FD72056E0B4964D8F020737F8,
+	GameManager__ctor_mF453CED520617BFB65C52405A964E06CF17DB368,
+	GameManager__cctor_m17C19843AAD22B2A037A6F670EEE26E7C2C35D0B,
+	LifePlayer_Start_m25392670802B17F57090D0B05AE1C912AADAC7E1,
+	LifePlayer_Update_m40EFCCAB30CFFB98C3AA7397011F1851E309497B,
+	LifePlayer_OnCollisionEnter_m6B431ED033487AC102244D8D322F32EB9F96BACC,
+	LifePlayer__ctor_mDD2D2940B64B2ABB14CB81913D33F9B7BFBA81C7,
+	CameraPointer_Update_mF19D284A593E4627CD4F22A58C11FB87B6627FF4,
+	CameraPointer__ctor_m5C4B443EB7C8A518306BF27BD947D938FDF5162D,
+	CardboardStartup_Start_m35E3138C09D62411C4C8503DD4D47DA2AF9BF6E2,
+	CardboardStartup_Update_m4D4D770B4F4BA807AB64DB40B6730226BB821883,
+	CardboardStartup__ctor_m4C459294D28BDCFC64FECC657FB860844F52D9F7,
+	ObjectController_Start_m50CBBCE72AB6C2A5C8BC1977D520CF3856A91E89,
+	ObjectController_TeleportRandomly_m45F23374832369044CBD4F60F4F976D33C97C181,
+	ObjectController_OnPointerEnter_m5D028066A6B28C2770924AE0C5A2A926E807FC42,
+	ObjectController_OnPointerExit_mB2F968E3432A4B5E90ABFCE7E6B025A39041809F,
+	ObjectController_OnPointerClick_m42836A892DD52FE0C121F5E37231BF198052017E,
+	ObjectController_SetMaterial_mF8B3638C2382300F585FCFAE6EC5626AAE5EC772,
+	ObjectController__ctor_mA008C8DFDEF1B6A05926338921FE1FD30BCEA9BC,
+	VrModeController_get__isScreenTouched_mD72F14326F1D19F3AC970FD0ED9D0C5CD5C6CFB5,
+	VrModeController_get__isVrModeEnabled_m8D8D68536E09FF2DEAD3795AF30A57BA6DF7C95F,
+	VrModeController_Start_m10BF77FE8B825D9640800D5DF7EF791A391FA49B,
+	VrModeController_Update_m43F984D5BF13F2C545B508D93645C1C3AC9262D5,
+	VrModeController_EnterVR_m5FD79BF38C6AF621AF9992378BC5C3618FDE5EFB,
+	VrModeController_ExitVR_m1FF25DFE5B3BC15CEDE5C42F56CC399693867397,
+	VrModeController_StartXR_m57D80B4AD6B28A7C4E36052563B6227E824CDDB4,
+	VrModeController_StopXR_m0338E8D8035C06E1A46F17C16BBC7933EE9CAA3A,
+	VrModeController__ctor_m94505259847FB7C54346D7CF02AD85793393D141,
+	U3CStartXRU3Ed__10__ctor_mDA431E1365B30DA2F64F1E7875CD387F2231E138,
+	U3CStartXRU3Ed__10_System_IDisposable_Dispose_m3DC1735AA5EC0D61A72803BB3B33B9499A373336,
+	U3CStartXRU3Ed__10_MoveNext_m9A6340456306ACFADD0D458946C82448A862E68B,
+	U3CStartXRU3Ed__10_System_Collections_Generic_IEnumeratorU3CSystem_ObjectU3E_get_Current_mD0B4CE8CBE980FD132B19F40FF2172502B1D0566,
+	U3CStartXRU3Ed__10_System_Collections_IEnumerator_Reset_mD99AEFDB38CFAC11731CDB371647891816288600,
+	U3CStartXRU3Ed__10_System_Collections_IEnumerator_get_Current_mD84D9ACDB0A04C619509BA51590975DB84C30B0F,
+	MouseLook_Start_m08FD16A47A96624540CD1B8EBA5FFAA1FD75E686,
+	MouseLook_Update_m18C0A114A78C19B888A9F74CA1FE5A837DF9E42A,
+	MouseLook__ctor_m4C8A79A3CB8D46D3E22E3AB1F645D41ED589312B,
+	ShootingProjectile_start_m8D5C605DDEC776B806C1D889CC103091E8B5E858,
+	ShootingProjectile_Update_mB492E4C25D212E97A17A8FE715E25501B5BF8ED6,
+	ShootingProjectile__ctor_mB3B10D0E79EA4841F424C1CFA2563C8D9F67C861,
+};
+static const int32_t s_InvokerIndices[54] = 
+{
+	3499,
+	3499,
+	2848,
+	3499,
+	3499,
+	3499,
+	2848,
+	3499,
+	3499,
+	3499,
+	2848,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	5313,
+	3499,
+	3499,
+	2848,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	2784,
+	3499,
+	3344,
+	3344,
+	3499,
+	3499,
+	3499,
+	3499,
+	3405,
+	3499,
+	3499,
+	2830,
+	3499,
+	3344,
+	3405,
+	3499,
+	3405,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+	3499,
+};
+IL2CPP_EXTERN_C const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule;
+const Il2CppCodeGenModule g_AssemblyU2DCSharp_CodeGenModule = 
+{
+	"Assembly-CSharp.dll",
+	54,
+	s_methodPointers,
+	0,
+	NULL,
+	s_InvokerIndices,
+	0,
+	NULL,
+	0,
+	NULL,
+	0,
+	NULL,
+	NULL,
+	NULL, // module initializer,
+	NULL,
+	NULL,
+	NULL,
+};
